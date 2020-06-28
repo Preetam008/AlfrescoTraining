db0070bd1724496614ac824b1d65db659132c07c
avenger008 <mahapatrauttam08@gmail.com>
2020-06-28 23:44:28 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { FileUploadComponent } from './component/file-upload/file-upload.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    FileUploadComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
